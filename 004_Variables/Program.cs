72b0282a78de83d39d24c7c56676a1af317f4093
alihakkidemiral <alihakkidemiral@gmail.com>
2023-08-14 00:08:20 +0300

    I know, I know... but i need practices for git using.

---
+++ 004_Variables/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName = "Korkut";
+            string userLastName = "Demiral";
+            Console.WriteLine(userName + " " + userLastName);
+            Console.ReadLine();
+        }
+    }
+}
